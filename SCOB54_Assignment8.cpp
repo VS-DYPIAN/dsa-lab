e65c56264d302eaacac82ed2323cac3b15e647bd
VS-DYPIAN <101723433+VS-DYPIAN@users.noreply.github.com>
2022-04-24 21:31:40 +0530

    Update SCOB54_Assignment8.cpp

--- SCOB54_Assignment8.cpp
+++ SCOB54_Assignment8.cpp
@@ -1,7 +1,8 @@
-/*mplement all the functions of a dictionary (ADT) using hashing and handle collisions 
+/*Implement all the functions of a dictionary (ADT) using hashing and handle collisions 
 using chaining with / without replacement. 
 Data: Set of (key, value) pairs, Keys are mapped to values, Keys must be comparable, 
-Keys must be unique. Standard Operations: Insert(key, value), Find(key), Delete(key)*/
+Keys must be unique. Standard Operations: Insert(key, value), Find(key), Delete(key)
+chain is not working*/
 
 #include<iostream>
 #include<cstring>
