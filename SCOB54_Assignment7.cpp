8ef710bfee987c3b38a1ebdb43c356aa2a4c6681
VS-DYPIAN <101723433+VS-DYPIAN@users.noreply.github.com>
2022-04-27 10:36:03 +0530

    Update SCOB54_Assignment7.cpp

--- SCOB54_Assignment7.cpp
+++ SCOB54_Assignment7.cpp
@@ -1,97 +1,209 @@
-/*
-Experiment 14 : There are flight paths between cities. If there is a flight between City A and City B then there is an edge between the cities. 
-The cost of the edge can be the time that flight take to reach city B from A, or the amount of fuel used for the journey. Represent this as a graph.
-The node can be represented by the airport name or name of the city. Use adjacency list representation of the graph or use adjacency matrix representation of the graph.
-*/
+/*Represent a given graph using adjacency matrix/list to perform DFS and using adjacency
+list to perform BFS. Use the map of the area around the college as the graph. Identify
+the prominent land marks as nodes and perform DFS and BFS on that.*/
 
-#include <iostream>
-#include <queue>
+#include<iostream>
 using namespace std;
 
-int adj_mat[50][50] = {0, 0};
-int visited[50] = {0};
 
-void dfs(int s, int n, string arr[])
+class Graph
 {
-    visited[s] = 1;
-    cout << arr[s] << " ";
-    for (int i = 0; i < n; i++)
-    {
-        if (adj_mat[s][i] && !visited[i])
-            dfs(i, n, arr);
-    }
-}
-
-void bfs(int s, int n, string arr[])
-{
-    bool visited[n];
-    for (int i = 0; i < n; i++)
-        visited[i] = false;
-    int v;
-    queue<int> bfsq;
-    if (!visited[s])
-    {
-        cout << arr[s] << " ";
-        bfsq.push(s);
-        visited[s] = true;
-        while (!bfsq.empty())
+        public :
+        	int mat[10][10];
+        	string vertex[10];
+        	int n,m,c;
+        	int stack[10];
+        	int E;
+            Graph()
+	        {
+	        	for(int i=0;i<10;i++)
+				{
+					for(int j=0;j<10;j++)
+					{
+						mat[i][j]=0;
+					}
+	        		
+				}
+	        	
+			}      
+        	
+        	
+        	
+        void Create()
         {
-            v = bfsq.front();
-            for (int i = 0; i < n; i++)
-            {
-                if (adj_mat[v][i] && !visited[i])
+        	cout<<"Enter no of land marks  in college :";
+        	cin>>n;
+        	
+        	
+        	
+        	for(int i=0;i<n;i++)
+        	{
+        		cout<<"Enter the name of land marks "<<i+1<<" :";
+        		cin>>vertex[i];
+			}
+			
+			for(int i=0;i<n;i++)
+			{
+				for(int j=0;j<n;j++)
                 {
-                    cout << arr[i] << " ";
-                    visited[i] = true;
-                    bfsq.push(i);
+				
+                    if(i==j)
+                    {
+                            mat[i][j]=0;
+                    }
+                                
+                    else if(mat[i][j]==0)
+                    {
+                            cout<<"Is there edge between land mark "<<vertex[i]<<" and land mark "<<vertex[j]<<"(If yes type 1 else 0)"<<" : ";
+                            cin>>mat[i][j];
+                            mat[j][i]=mat[i][j];
+							
+                              
+                    }
+                                
                 }
             }
-            bfsq.pop();
-        }
-    }
-}
+		}
+			
+			
+		
+		void Display()
+	    {
+	                cout<<"\n";
+	                for(int i=0;i<n;i++)
+	                {
+	                        cout<<"\t"<<vertex[i];
+	                }
+	                
+	                
+	                for(int i=0;i<n;i++)
+	                {
+	                        cout<<"\n"<<vertex[i];;
+	                        
+	                        
+	                        
+	                        for(int j=0;j<n;j++)
+	                        {
+	                                
+	                                cout<<"\t"<<mat[i][j];
+	                        }
+	                        
+	                }
+	    }
+	        
+	    void DFS()
+	    {
+	        	  
+			    int vis[n];
+			    int stack[2*n];
+			    int top=0;
+			    for(int i=0;i<n;i++)
+				{
+			        vis[i] = 0;
+			    }
+			
+			    stack[top] = 0;
+			    
+			
+			    while(top!=-1){
+			        int node = stack[top];
+			        top--;
+			
+			        if(vis[node]==0){
+			            cout<<vertex[node]<<" ";
+			            vis[node] = 1;
+			            for(int i=0;i<n;i++){
+			                if(mat[node][i]!=0){
+			                    top++;
+			                    stack[top] = i;
+			                }
+			            }
+			        }
+			
+			    }
+			    cout<<endl;
+	    }
+		
+		void BFS()
+		{
+
+		    int vis[n];
+		    int queue[2*n];
+		    int front = 0,rear=0;
+		    for(int i=0;i<n;i++)
+			{
+		        vis[i] = 0;
+		    }
+		    queue[rear] = 0;
+		    vis[0] = 1;
+		    rear++;
+		
+		    while(front!=rear)
+			{
+		        //  int size = rear+1;
+		        
+		        int node = queue[front];
+		        front++;
+		        cout<<vertex[node]<<" ";
+		        for(int i=0;i<n;i++){
+		            if(mat[node][i]==1 and vis[i] == 0)
+					{
+		                queue[rear] = i;
+		                rear++;
+		                vis[i] = 1;
+		            }
+		        }
+		    }
+		    cout<<endl;
+		}                
+};
+
 
 int main()
+
 {
-    cout << "Enter no. of cities: ";
-    int n, u;
-    cin >> n;
-    string cities[n];
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Enter city #" << i << " (Airport Code): ";
-        cin >> cities[i];
+ Graph g;
+ int x;
+  while (1)
+   {
+      cout<<"\n****************************"<<endl;
+      cout<<"Operations on Graph"<<endl;
+      cout<<"****************************"<<endl;
+      cout<<"1.Create Graph using adjacency matrix"<<endl;
+      cout<<"2.Display the Graph" <<endl;
+      cout<<"3.DFS sequence of graph " <<endl;
+      cout<<"4.BFS sequence of graph " <<endl;
+      cout<<"5.Exit" <<endl;
+      cout<<"Enter your choice :-";
+      cin>>x;
+      switch(x)
+      {
+         case 1:
+            g.Create();
+           
+            break;
+         
+         case 2:
+            g.Display();
+            break;
+         
+         case 3:
+           cout<<"DFS sequence of graph is :";
+           g.DFS();
+           break;
+           
+         case 4:
+           cout<<"BFS sequence of graph is :";
+           g.BFS();
+           break;
+           
+         case 5:
+           
+            exit(1);
+           
+         default:
+            cout<<"Wrong choice"<<endl; 
+      }
     }
-    
-    cout << "\nYour cities are: " << endl;
-    for (int i = 0; i < n; i++)
-        cout << "city #" << i << ": " << cities[i] << endl;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            cout << "Enter distance between " << cities[i] << " and " << cities[j] << " : ";
-            cin >> adj_mat[i][j];
-            adj_mat[j][i] = adj_mat[i][j];
-        }
-    }
-    cout << endl;
-    for (int i = 0; i < n; i++)
-        cout << "\t" << cities[i] << "\t";
-    for (int i = 0; i < n; i++)
-    {
-        cout << "\n"
-             << cities[i];
-        for (int j = 0; j < n; j++)
-            cout << "\t" << adj_mat[i][j] << "\t";
-        cout << endl;
-    }
-    cout << "Enter Starting Vertex: ";
-    cin >> u;
-    cout << "DFS: ";
-    dfs(u, n, cities);
-    cout << endl;
-    cout << "BFS: ";
-    bfs(u, n, cities);
-    return 0;
+
 }
