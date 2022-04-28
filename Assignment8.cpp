d4624b7b5111cb412b77fbb3adf5360a33263a18
VS-DYPIAN <101723433+VS-DYPIAN@users.noreply.github.com>
2022-04-28 19:23:11 +0530

    Update Assignment8.cpp

--- Assignment8.cpp
+++ Assignment8.cpp
@@ -1,192 +1,193 @@
+/*									******* Assignment No 8 *******
+Implement all the functions of a dictionary (ADT) using hashing and handle collisions 
+using chaining with / without replacement. 
+Data: Set of (key, value) pairs, Keys are mapped to values, Keys must be comparable, 
+Keys must be unique. Standard Operations: Insert(key, value), Find(key), Delete(key)
+*/
+
 #include<iostream>
-#include<cstring>
-#define max 10
+#include<string.h>
+
 using namespace std;
 
-struct Dict
-{
-  public:
-	char word[20];
-	char meaning[20];
+class Node{
+public:
 	int chain;
+	string word;
+	string mean;
+}obj[10];
 
-}; 	
-
-class Hashfunction
-{
-	public:
-		Dict ht[max];               //ht is array of type class dict
-		char w[30],m[30];
-		Hashfunction()	
-		{
-			for(int i=0;i<max;i++)
-			{
-				strcpy(ht[i].word,"-1");
-				strcpy(ht[i].meaning,"-1");
-				ht[max].chain= -1;
-			}
-		}
-		
-		int hash(char ckey[10])
-		{
-			int i,s=0,c=0,x=0;    // string to ascii conversion
-			for(i=0;ckey[i]!='\0';i++)   // "/0" is used to check end of string
-			{
-				s=s+ckey[i];
-			}
-            x= s%max;
-            c=x;
-			return(c);
-			while(1)
-           {
-
-                        if(strcmp(ht[x].word,"-1")==0)
-                    {
-                        strcpy(ht[x].word,w);
-                        strcpy(ht[x].meaning,m);
-                        if(c!=x)
-                        {
-                            ht[c].chain=x;
-                        }
-                        break;
-                    }
-                    else
-                         x=(x+1)%10;
-
-                        if(c==x)
-                        {  cout<<"\n hash table is full";
-                            break;
-                        }
-            }
-			
-		}
-		
-		void insert(Dict d);
-		void display();
-		int search(char cW[]);
-		void delword(char cW[]);
-		
-};
-
-void Hashfunction::insert(Dict d)
-{
-	int iIndex=10;
-	for(int i=0;i%max!=iIndex;i=(i+1)%max)
-	{
-		iIndex=(hash(d.word)+i*i)%max;
-		//cout<<"\n\n Position :"<<" "<<i<<" " << "-1"<<" "<<iIndex<<endl;
-		if(i>0)
-		
-		//cout<<"\n Collision is at "<<iIndex<<endl; 
-		
-		if(strcmp(ht[iIndex].word,"-1")==0)
-		{
-			ht[iIndex]=d;
-			break;
-		}
-	}	
-		
+int hash_func(string wd){
+	int key=0;
+	for(int i=0;i<wd.size();i++){
+		key += wd[i];
+	}
+	return key%10;
 }
 
-void Hashfunction::display()
-{
-	cout<<"index\t\tWord\t\tmeaning\t\tchain";
-	for(int i=0;i<max;i++)
-	{
-		cout<<"\n"<<i<<"\t\t"<<ht[i].word<<"\t\t"<<ht[i].meaning<<"\t\t"<<ht[max].chain<<"\n";
-	}
+void collision(int key, string wd, string mn){
+	int i=1;
 
-}
-
-int Hashfunction::search(char cW[10])
-{
-	int iIndex,iFlag=0;
-	for(int i=0;i%max!=iIndex;i=(i+1)%max)
-	{
-		iIndex=(hash(cW)+i*i)%max;
-		if(strcmp(ht[iIndex].word,cW)==0)
-		{
-			cout<<"Word Found and Meaning is :"<<ht[iIndex].meaning<<endl;
-			iFlag=1;
+	while(((key+i) % 10) < 10){
+		if(obj[(key+i)%10].word == "-"){
+			obj[(key+i) % 10].word = wd;
+			obj[(key+i) % 10].mean = mn;
+			obj[(key+i-1)%10].chain = (key+i)%10;
 			break;
 		}
-	}
-	if(iFlag==0)
-	cout<<"Word Not Found"<<endl;
-	return 0;
-}
-
-void Hashfunction::delword(char cW[10])
-{
-	int iIndex,iFlag=0;
-	for(int i=0;i%max!=iIndex;i=(i+1)%max)
-	{
-		iIndex=(hash(cW)+i*i)%max;
-		if(strcmp(ht[iIndex].word,cW)==0)
-		{
-			cout<<"\nWord Found and delword :"<<ht[iIndex].meaning<<endl;
-			strcpy(ht[iIndex].word,"-1");
-			strcpy(ht[iIndex].meaning,"-1");
-			iFlag=1;
-			break;
+		else{
+			i++;		
 		}
 	}
-	if(iFlag==0)
-	cout<<"Word Not Found"<<endl;
+}
+
+void hash_ini(){
+
+	for(int i=0;i<10;i++){
+		obj[i].word = "-";
+		obj[i].mean = "-";
+		obj[i].chain = -1;
+	}
+}
+
+void hash_table(){
+	
+	
+	string wd,mn;
+	cout<<" Enter a word :- ";
+	cin>>wd;
+	cout<<endl;
+	cout<<"Enter Meaning for word :- ";
+	cin>>mn;
+	cout<<endl;
+	
+	int hash_key = hash_func(wd);
+	
+	if(obj[hash_key].word == "-"){
+		obj[hash_key].word = wd;
+		obj[hash_key].mean = mn;
+	}
+	else{
+		collision(hash_key, wd, mn);
+	}
 	
 }
-	
-int main()
-{
-	char cW[10];
-	int x,iFlag=0;
-	Hashfunction h;
-	Dict d;
-	while(1)
-	{
-	      cout<<"**********"<<endl;
-	      cout<<"Operations on dictionary using hashtable"<<endl;
-	      cout<<"**********"<<endl;
-	      cout<<"1.Insert "<<endl;
-	      cout<<"2.Display Dictionary" <<endl;
-	      cout<<"3.Search " <<endl;
-	      cout<<"4.Delete" <<endl;
-	      cout<<"5.Exit" <<endl;
-	      cout<<"Enter your choice :-";
-	      cin>>x;
-                switch(x)
-		{
-			case 1:
-				cout<<"Enter word:";
-				cin>>d.word;
-				cout<<"Enter Meaning:";
-				cin>>d.meaning;
-				h.insert(d);
-				break;
-			
-			case 2:
-				h.display();
-				break;
-				
-			case 3:
-				cout<<"\n Enter word to be searched:";
-				cin>>cW;
-				h.search(cW);
-				break;
-				
-			case 4:
-				cout<<"\n Enter the word to be deleted:";
-				cin>>cW;
-				h.delword(cW);
-				break;
-				
-			case 5:
-				exit(1);
-			
-			default:
-                cout<<"Wrong choice"<<endl;
-				
-		}
-	}			
 
+void display(){
+    cout<<"index"<<"\t"<<"word"<<"\t"<<"meaning"<<"\t"<<"chain"<<endl;
+    for(int i=0;i<10;i++){
+        cout<<i<<"\t"<<obj[i].word<<"\t"<<obj[i].mean<<"\t"<<obj[i].chain<<endl;
+    }
+}
+
+void del_key(string word){
+	int key = hash_func(word);
+// what if number is present but it also contains a chain? what can we 
+// do how to store the chain or how to replace the contents of chain
+
+	if(obj[key].word == word){
+		obj[key].word = "-";
+		obj[key].mean = "-";
+		obj[key].chain = -1;
+	}
+	else if(obj[key].word != word){
+	    int target = obj[key].chain;
+		while(true){
+			if(obj[target].word == word){
+				obj[target-1].chain = obj[target].chain;
+				obj[target].word = "-";
+				obj[target].mean = "-";
+				obj[target].chain = -1;
+				cout<<"DEletion successful"<<endl;
+				break;
+			}
+			target = obj[target].chain;
+		}
+	}
+	else{
+		cout<<"word not found"<<endl;
+	}
+}
+
+void find(string word){
+	int key = hash_func(word);
+
+	if(obj[key].word == word ){
+		cout<<"Found the Word"<<endl;
+		cout<<obj[key].word<<" --> "<<obj[key].mean<<endl;
+	}
+	else if(obj[key].chain != -1){
+
+		int target = obj[key].chain;
+		while(true){
+			if(obj[target].word == word){
+				cout<<"Found the Word"<<endl;
+				cout<<obj[target].word<<" --> "<<obj[target].mean<<endl;
+				break;
+			}
+			target = obj[target].chain;
+		}
+	}
+	else{
+		cout<<"Not Found"<<endl;
+	}
+
+}
+
+int main(){
+	
+	int ch;
+	string del_wd;
+	string src;
+	hash_ini();
+
+do{
+	cout<<"********* Enter Your Choice **********"<<endl;
+	cout<<"1. Insert data in hash table"<<endl;
+	cout<<"2. Display Hash Table"<<endl;
+	cout<<"3. Delete word from Hash Table"<<endl;
+	cout<<"4. Find word in a Hash Table"<<endl;
+
+	cout<<"5. Exit"<<endl;
+	cout<<"Enter your choice :- ";
+	cin>>ch;
+	cout<<endl;
+
+	switch (ch)
+	{
+	case 1:
+		cout<<"Enter no of entries you want to make :- ";
+		int n;
+		cin>>n;
+		for(int i=0;i<n;i++){
+			hash_table();
+		}
+		break;
+	
+	case 2:
+		display();
+		break;
+	
+	case 3:
+		cout<<"Enter the word you want to delete :- ";
+		
+		cin>>del_wd;
+		del_key(del_wd);
+		break;
+
+	case 4:
+		cout<<"Enter the word you want to search :- ";
+		
+		cin>>src;
+		find(src);
+		break;
+	
+
+	}
+
+}while(ch<5);
+
+    cout<<"Done Successfully"<<endl;
+
+return 0;
 }
