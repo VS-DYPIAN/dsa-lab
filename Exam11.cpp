24d99b49c4e9a792d614f8f0615ebaf119966b20
VS-DYPIAN <101723433+VS-DYPIAN@users.noreply.github.com>
2022-05-15 19:45:59 +0530

    Update Exam11.cpp

--- Exam11.cpp
+++ Exam11.cpp
@@ -1,36 +1,35 @@
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void con_obst(void);
-void print(int,int);
+void obst(void);
+void print(int, int);
 
-float a[20],b[20],wt[20][20],c[20][20];
+float p[20],q[20],wt[20][20],c[20][20];
 int r[20][20],n;
 
-
-void con_obst(void)
+void obst(void)
 {
-    int i,j,k,l,min;
-    for(i=0;i<n;i++)
-         { //Initialisation
-            c[i][i]=0.0;
-            r[i][i]=0;
-            wt[i][i]=b[i];
-            // for j-i=1 can be j=i+1
-            wt[i][i+1]=b[i]+b[i+1]+a[i+1];
-            c[i][i+1]=b[i]+b[i+1]+a[i+1];
-            r[i][i+1]=i+1;
-         }
-    c[n][n]=0.0;
-    r[n][n]=0;
-    wt[n][n]=b[n];
-    //for j-i=2,3,4....,n
-    for(i=2;i<=n;i++)
-     {
-        for(j=0;j<=n-i;j++)
-         {
-            wt[j][j+i]=b[j+i]+a[j+i]+wt[j][j+i-1];
+  int i,j,k,l,min;
+  for(int i=0; i<n;i++)
+  {
+      r[i][i] = 0;
+      wt[i][i] =q[i];
+      c[i][i] = 0.0;
+      
+      r[i][i+1] = i+1;
+      wt[i][i+1] = q[i]+q[i+1]+p[i+1];
+      c[i][i+1] = q[i]+q[i+1]+p[i+1];
+      
+  }
+  r[n][n] = 0;
+  c[n][n] =0.0;
+  wt[n][n]= q[n];
+  
+  for(i=2 ;i<=n ;i++)
+  {
+      for(j=0;j<=n-1 ;j++)
+      {
+         wt[j][j+i]=q[j+i]+p[j+i]+wt[j][j+i-1];
             c[j][j+i]=9999;
             for(l=j+1;l<=j+i;l++)
              {
@@ -41,18 +40,17 @@ void con_obst(void)
                     r[j][j+i]=l;
                  }
              }
-            c[j][j+i]+=wt[j][j+i];
-         }
-        cout<<endl;
-     }
-    
+            c[j][j+i]+=wt[j][j+i];  
+      }
+      cout<<endl;
+  }
     cout<<"Optimal BST is :: ";
     cout<<"w[0]["<<n<<"] :: "<<wt[0][n]<<endl;
     cout<<"c[0]["<<n<<"] :: "<<c[0][n]<<endl;;
     cout<<"r[0]["<<n<<"] :: "<<r[0][n]<<endl;;
 }
 
-void print(int l1,int r1)
+void print(int l1, int r1)
 {
     if(l1>=r1)
     return;
@@ -63,33 +61,29 @@ void print(int l1,int r1)
     print(l1,r[l1][r1]-1);
     print(r[l1][r1],r1);
     return;
-}
+}   
+
 
 int main()
 {
-    int i;
-    cout<<"****** PROGRAM FOR OBST ******"<<endl;
-    cout<<"Enter the no. of nodes : ";
+    cout<<"****Optimal binary search tree****"<<endl;
+    cout<<endl;
+    cout<<"Enter the number of keys : ";
     cin>>n;
-    cout<<"Enter the probability for successful search :: "<<endl;
-    cout<<endl;
+    cout<<"Enter the probability for successful search : "<<endl;
+    for(int i=1;i<=n; i++)
+    {
+        cout<<"p["<<i<<"]"<<" = ";
+        cin>>p[i];
+    }
+    cout<<"Enter the probability for unsuccessful search : "<<endl;
+    for(int i=0;i<=n; i++)
+    {
+        cout<<"q["<<i<<"]"<<" = ";
+        cin>>q[i];
+    }
     
-    for(i=1;i<=n;i++)
-     {
-        cout<<"p["<<i<<"]";
-        cin>>a[i];
-     }
-    
-    cout<<"Enter the probability for unsuccessful search :: "<<endl;
-    cout<<endl;
-    
-    for(i=0;i<=n;i++)
-     {
-        cout<<"q["<<i<<"]";
-        cin>>b[i];
-     }
-    
-    con_obst();
+    obst();
     print(0,n);
     cout<<endl;
 }
